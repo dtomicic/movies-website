8f862bbfc6b71c165fffb976dde5242119593783
unknown <dtomicic192@gmail.com>
2023-04-24 13:36:37 +0200

    Added banner picture and text

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,12 +1,24 @@
 import Image from "next/image";
-import { BannerStyled } from "./style";
+import { BannerStyled, HeroText } from "./style";
 
 const Banner = () => {
 	return (
-        <BannerStyled>
-            <Image src="/images/hero.jpg" alt="hero" width={4740} height={3164} loading="eager" />
-        </BannerStyled>
-    );
+		<BannerStyled>
+			<Image
+				src="/images/hero.jpg"
+				alt="hero"
+				width={4740}
+				height={3164}
+				loading="eager"
+				priority={true}
+			/>
+			<HeroText>
+				<h1>
+					Explore your favourite movies and TV shows all in <span>one</span> place
+				</h1>
+			</HeroText>
+		</BannerStyled>
+	);
 };
 
 export default Banner;
--- components/Banner/style.ts
+++ components/Banner/style.ts
@@ -1,8 +1,12 @@
+import { mixins } from "@/styles/styles";
 import styled from "styled-components";
 
 export const BannerStyled = styled.div`
 	display: flex;
+    align-items: center;
+    justify-content: center;
 	height: 185px;
+    position: relative;
 
 	img {
         object-fit: cover;
@@ -10,3 +14,16 @@ export const BannerStyled = styled.div`
 		height: 100%;
 	}
 `;
+
+export const HeroText = styled.div`
+    ${mixins.contentWide};
+    position: absolute;
+
+    h1 {
+        text-align: center;
+        color: white;
+        font-family: "Montserrat", sans-serif;
+        font-size: 20px;
+        font-weight: 700;
+    }
+`
