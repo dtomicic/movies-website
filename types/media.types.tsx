767fe56d3ed3cbe5ca11426ff650ad1a2f234b6e
unknown <dtomicic192@gmail.com>
2023-04-27 23:14:09 +0200

    Made api call

---
+++ types/media.types.tsx
@@ -0,0 +1,11 @@
+export interface ITrendingMovie {
+    title: string;
+    overview: string;
+    poster_path: string;
+}
+
+export interface ITrendingShow {
+    name: string;
+    overview: string;
+    poster_path: string;
+}
