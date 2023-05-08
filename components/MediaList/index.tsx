c11d3faa4e4bacebec97b485ed52b03f8583111d
Darko Tomičić <Darko.Tomicic@convergent-usa.com>
2023-05-08 15:01:32 +0200

    Started creating media details page

--- components/MediaList/index.tsx
+++ components/MediaList/index.tsx
@@ -1,32 +1,41 @@
 import { ITrending } from "@/types/media.types";
 import { MediaListStyled } from "./style";
 import Image from "next/image";
+import Link from "next/link";
 
 interface IMediaListProps {
-	media: ITrending[];
+    media: ITrending[];
 }
 
 const MediaList = (props: IMediaListProps) => {
-	const { media } = props;
-	return (
-		<MediaListStyled>
-			{media.map((item, index) => (
-				<div key={index}>
-                    <div className="overlay">
-                        <span>{item.title ? item.title : item.name}</span>
-                    </div>
-					<Image
-						src={`http://image.tmdb.org/t/p/w500/${item.poster_path}`}
-						alt={`Movie poster for ${item.title}`}
-						width={500}
-						height={750}
-                        priority={index === 0}
-                        loading={index === 0 ? "eager" : "lazy"}
-					/>
-				</div>
-			))}
-		</MediaListStyled>
-	);
+    const { media } = props;
+    return (
+        <MediaListStyled>
+            {media.map((item, index) => (
+                <div key={index}>
+                    <Link
+                        href={
+                            item.media_type === "movie"
+                                ? `/movie/${item.id}`
+                                : `/tv/${item.id}`
+                        }
+                    >
+                        <div className="overlay">
+                            <span>{item.title ? item.title : item.name}</span>
+                        </div>
+                        <Image
+                            src={`http://image.tmdb.org/t/p/w500/${item.poster_path}`}
+                            alt={`Movie poster for ${item.title}`}
+                            width={500}
+                            height={750}
+                            priority={index === 0}
+                            loading={index === 0 ? "eager" : "lazy"}
+                        />
+                    </Link>
+                </div>
+            ))}
+        </MediaListStyled>
+    );
 };
 
 export default MediaList;
